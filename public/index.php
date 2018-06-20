0a100469ecd40a80e6a0a11f44d55ed35edd3baf
test1 <test1@mail.me>
2018-06-20 18:00:35 +0200

    core init

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 
 // load core starter
 $rootDir = realpath(__DIR__."/../../");
+
 require_once("$rootDir/fusion-code/core/starter.php");
 require_once("$rootDir/fusion-plugin/index.php");
 require_once("$rootDir/fusion-theme/index.php");
 
-new Core(__DIR__);
+new Site(__DIR__);
